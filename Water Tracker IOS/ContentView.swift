50a3bb6be360a2820a844203b069e54b441cde11
Libroru <47794032+Libroru@users.noreply.github.com>
2023-06-18 17:41:18 +0200

    Using AppStorage variables + Settings Menu finished

--- Water Tracker IOS/ContentView.swift
+++ Water Tracker IOS/ContentView.swift
@@ -20,18 +20,10 @@ enum UnitType: String, CaseIterable, Identifiable {
 }
 
 struct ContentView: View {
-    @State public var toAdd: String = ""
-    @State public var waterLevel: Double = 0
-    @State public var currentWaterLevel: String = ""
-    @State public var dailyGoal: Double = 3000
-
-    @State private var unitPickerSelection: UnitType = UnitType.Liters
-    
-    @AppStorage("CURRENT_LEVEL_KEY") var savedCurrentLevel: Double = 0
-    @AppStorage("AMOUNT_TO_ADD_KEY") var savedAmountToAdd: String = ""
-    @AppStorage("GOAL_KEY") var savedGoal: Double = 0
-    
-    @AppStorage("SETTINGS_UNITS_KEY") var savedUnitType: UnitType = UnitType.Liters
+    @AppStorage("CURRENT_LEVEL_KEY") var waterLevel: Double = 0
+    @AppStorage("AMOUNT_TO_ADD_KEY") var amountToAdd: String = ""
+    @AppStorage("GOAL_KEY") var dailyGoal: String = "3L"
+    @AppStorage("SETTINGS_UNITS_KEY") var unitPickerSelection: UnitType = UnitType.Liters
     
     @Environment(\.scenePhase) private var scenePhase
     
@@ -55,11 +47,12 @@ struct ContentView: View {
                             }
                             Form {
                                 Section(header: Text("General Settings")) {
-                                    Picker("Unit", selection: $savedUnitType) {
+                                    Picker("Unit", selection: $unitPickerSelection) {
                                         ForEach(UnitType.allCases) { option in
                                             Text(String(describing: option))
                                         }
                                     }
+                                    TextField("Daily Goal", text: $dailyGoal)
                                 }
                             }
                         }
@@ -80,36 +73,17 @@ struct ContentView: View {
                 
                 ZStack {
                     CircularProgressBar(progress: {
-                        (waterLevel / dailyGoal)
+                        (waterLevel / strip_of_unit(input: dailyGoal))
                     }(), strokeWidth: 20, progressColor: Color.blue, backdropColor: Color.cyan)
                         .frame(width: 200)
-                        .onAppear {
-                            if (savedGoal != 0) {
-                                dailyGoal = savedGoal
-                            }
-                        }
-                    Text(currentWaterLevel)
+                    Text(format_string(input: waterLevel, selection: unitPickerSelection))
                         .font(.system(size: 35))
-                        .onAppear {
-                            //unitPickerSelection = savedUnitType
-                            
-                            currentWaterLevel = format_string(input: savedCurrentLevel, selection: unitPickerSelection)
-                            waterLevel = savedCurrentLevel
-                        }
-                        .onChange(of: scenePhase) { phase in
-                            if phase == .inactive || phase == .background {
-                                savedCurrentLevel = waterLevel
-                                savedUnitType = unitPickerSelection
-                            }
-                        }
                 }
                 Spacer()
                     .frame(height: 50)
                 HStack {
                     Button {
-                        waterLevel -= strip_of_unit(input: toAdd, selection: unitPickerSelection)
-                        
-                        currentWaterLevel = format_string(input: waterLevel, selection: unitPickerSelection)
+                        waterLevel -= strip_of_unit(input: amountToAdd)
                     } label: {
                         Text("-")
                             .padding(5)
@@ -118,28 +92,16 @@ struct ContentView: View {
                     .buttonStyle(BorderedProminentButtonStyle())
                     .font(.system(size: 25))
                     
-                    TextField(text: $toAdd,
+                    TextField(text: $amountToAdd,
                               prompt: Text(unitPickerSelection == UnitType.Liters ? "500ml" : "16oz")) {
                         Text("Amount")
                     }
                     .frame(width: 125, height: 45)
                     .multilineTextAlignment(.center)
                     .font(.system(size: 25))
-                    .onAppear {
-                        if (savedAmountToAdd != "") {
-                            toAdd = savedAmountToAdd
-                        }
-                    }
-                    .onChange(of: scenePhase) { phase in
-                        if phase == .inactive || phase == .background {
-                            savedAmountToAdd = toAdd
-                        }
-                    }
                     
                     Button {
-                        waterLevel += strip_of_unit(input: toAdd, selection: unitPickerSelection)
-                        
-                        currentWaterLevel = format_string(input: waterLevel, selection: unitPickerSelection)
+                        waterLevel += strip_of_unit(input: amountToAdd)
                     } label: {
                         Text("+")
                             .padding(5)
@@ -164,7 +126,7 @@ struct ContentView_Previews: PreviewProvider {
 func format_string(input: Double, selection: UnitType) -> String {
     if (selection == UnitType.Liters) {
         if ((input / 1000) >= 1.0) {
-            return String(input / 1000) + "L"
+            return String(format: "%3.1f", input / 1000) + "L"
         } else {
             return String(format: "%3.0f", input) + "ml"
         }
@@ -174,18 +136,14 @@ func format_string(input: Double, selection: UnitType) -> String {
 }
 
 /// Does the opposite to `format_string()`, strips away all units and returns the clean Double
-func strip_of_unit(input: String, selection: UnitType) -> Double {
-    if (selection == UnitType.Liters) {
-        if (input.hasSuffix("ml") == true) {
-            return Double(input.replacingOccurrences(of: "ml", with: ""))!
-        } else {
-            return Double(input.replacingOccurrences(of: "L", with: ""))!
-        }
+func strip_of_unit(input: String) -> Double {
+    if (input.hasSuffix("ml")) {
+        return Double(input.replacingOccurrences(of: "ml", with: ""))!
+    } else if (input.hasSuffix("L")) {
+        return Double(input.replacingOccurrences(of: "L", with: ""))!
+    } else if (input.hasSuffix("oz")) {
+        return Double(Double(input.replacingOccurrences(of: "oz", with: ""))! * 29.574)
     } else {
-        if (input.hasSuffix("ml") == true) {
-            return Double(input.replacingOccurrences(of: "ml", with: ""))!
-        } else {
-            return Double(Double(input.replacingOccurrences(of: "oz", with: ""))! * 29.574)
-        }
+        return 3000.0
     }
 }
